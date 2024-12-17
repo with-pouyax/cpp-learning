41f0285001d8db8b26bacde890636d036c8951c7
pouyax <75066687+with-pouyax@users.noreply.github.com>
2024-12-17 20:50:27 +0000

    finished first lesson

---
+++ 01. First c++ programm.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello world"; //in c++ we use cout to print something on console
+                         // cout is an object of ostream class in iostream library
+                         // << is called insertion operator
+                         // we have to put our text in double quotes, when we want to print something on console
+                         // we can also use single quotes, but it is not recommended
+                          
+    return 0;
+}
