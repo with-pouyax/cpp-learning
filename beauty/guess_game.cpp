3af1d6b954b6df4e0e160fffbda8dac25de47c17
pouyax <75066687+with-pouyax@users.noreply.github.com>
2025-01-27 19:13:58 +0000

    2:57

---
+++ beauty/guess_game.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+    int hotUserNum, guestUserNum;
+
+    std::cout << "please enter a number: " << std::endl;
+    std::cin >> hotUserNum;
+
+    system("clear");
+
+    std::cout << "please enter your guess: " << std::endl;
+    std::cin >> guestUserNum;
+
+    if (hotUserNum == guestUserNum)
+        std::cout << "correct";
+    else
+        std::cout << "failed";
+
+}
