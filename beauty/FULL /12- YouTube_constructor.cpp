ca599eb2a57a02361ea29595f58f1a0c7e556585
pouya ghajardavalou <pouyax@pouyas-iMac.local>
2025-02-10 20:35:14 +0100

    13- OOP Encapsulation.cpp

--- beauty/FULL /12- YouTube_constructor.cpp
+++ beauty/FULL /12- YouTube_constructor.cpp
@@ -2,24 +2,45 @@
 #include <list>
 
 class YouTubeChannel {
-public:
+private:
 	std::string Name;
 	std::string OwnerName;
 	int SubscribersCount;
 	std::list<std::string> PublishedVideoTitles;
-
+public:
 	YouTubeChannel(std::string name, std::string ownerName){
 		name = name;
 		ownerName = ownerName;
 		SubscribersCount = 0;
 	}
 
+	void name(std::string name){
+		Name = name;
+	}
+
+	void owner(std::string name){
+		OwnerName = name;
+	}
+
+	void Subscribe(){
+		SubscribersCount++;
+	}
+
+	void Unsubscribe(){
+		if(SubscribersCount > 0)
+			SubscribersCount--;
+	}
+
+	void PublishedVideo(std::string title) {
+		PublishedVideoTitles.push_back(title);
+	}
+
 	void GetInfo()
 	{
 		std::cout << "Name: " << Name << std::endl;
-		std::cout << "Name: " << OwnerName << std::endl;
-		std::cout << "Name: " << SubscribersCount << std::endl;
-		std::cout << "videos:" << std::endl;
+		std::cout << "Owner: " << OwnerName << std::endl;
+		std::cout << "Subscribers Number: " << SubscribersCount << std::endl;
+		std::cout << "videos: " << std::endl;
 		for(std::string videoTitle : PublishedVideoTitles ) // this is only possible in c++11 and later
 			std::cout << videoTitle << std::endl;
 	}
@@ -30,10 +51,15 @@ public:
 int main()
 {
 	YouTubeChannel ytChannel("with.pouyax", "pouya");
-	
-	ytChannel.PublishedVideoTitles.push_back("who am I ?");
-	ytChannel.PublishedVideoTitles.push_back("lets go");
-	ytChannel.PublishedVideoTitles.push_back("first interview");
+	ytChannel.name("42");
+	ytChannel.owner("Marta");
+	ytChannel.Subscribe();
+	ytChannel.Subscribe();
+	ytChannel.Subscribe();
+	ytChannel.Unsubscribe();
+	ytChannel.PublishedVideo("hi");
+	ytChannel.PublishedVideo("bye");
+
 	
 	ytChannel.GetInfo();
 }
